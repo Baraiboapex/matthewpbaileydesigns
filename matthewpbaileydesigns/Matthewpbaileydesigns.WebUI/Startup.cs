706ae5f051d1d7203aae8c78af8c646e4b3cf37c
Matthew Bailey <MBailey@jtafla.com>
2020-07-16 10:12:07 -0400

    MatthewPBaileyDesigns Initial Solution

---
+++ matthewpbaileydesigns/Matthewpbaileydesigns.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Matthewpbaileydesigns.WebUI.Startup))]
+namespace Matthewpbaileydesigns.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
